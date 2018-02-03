a24b6654ae7bc2b879257d112ed7ebdc39c7769f
Vignesh Hari <vichuhari100@gmail.com>
2018-02-04 00:42:16 +0530

    Resolved Bug

--- innathe_parupadi/innathe_parupadi/settings.py
+++ innathe_parupadi/innathe_parupadi/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 MEDIA_ROOT = os.path.join(BASE_DIR , "media/")
 MEDIA_URL = "/media/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),]
+    os.path.join(BASE_DIR, "staticfiles/"),]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR , "static/")
 
