4041c2b2f247fc385dc330db553ddc0abd004e03
Vignesh Hari <vichuhari100@gmail.com>
2018-02-04 00:44:02 +0530

    Resolved Bug

--- innathe_parupadi/innathe_parupadi/settings.py
+++ innathe_parupadi/innathe_parupadi/settings.py
@@ -72,10 +72,12 @@ TEMPLATES = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR , "media/")
 MEDIA_URL = "/media/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles/"),]
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR , "static/")
+STATIC_ROOT= "staticfiles"
+STATIC_URL = 'static'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 WSGI_APPLICATION = 'innathe_parupadi.wsgi.application'
 
